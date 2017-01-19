b475ba2de545121989dc5b8e85867f3b56ca241f
Hannah Nordgren <hey@hereshannahs.info>
2017-01-19 09:09:59 -0500

    starting on gallery page with modal

--- js/gallery.js
+++ js/gallery.js
@@ -1,11 +1,15 @@
 var buttons=$('.artistimage');
+<<<<<<< HEAD
 var modal = $('.gallery-modal');
 var closeModal = $('.modal-close');
 var url = 'localhost:8000/galleryUncompressed.html;'
+=======
+>>>>>>> starting on gallery page with modal
 
 function modalPopup(){
   var eachImg = $(this).attr('id').split('-')[1];
 
+<<<<<<< HEAD
  $.ajax({
       url: url,
       type: "POST",
@@ -33,3 +37,8 @@ function hideModal(){
 $(buttons).click(modalPopup);
 
 $(closeModal).click(hideModal);
+=======
+}
+
+$(buttons).click(modalPopup);
+>>>>>>> starting on gallery page with modal
