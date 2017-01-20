d5001a519a29551c8bcc69ceb4a3b042a12c07b9
Hannah Nordgren <hey@hereshannahs.info>
2017-01-20 09:06:32 -0500

    initial popul modal, needs much work

--- js/gallery.js
+++ js/gallery.js
@@ -1,7 +1,32 @@
 var buttons=$('.artistimage');
+var modal = $('.modal');
+var closeModal = $('.modal-close');
 
 function modalPopup(){
   var eachImg = $(this).attr('id').split('-')[1];
+  $(modal).css('display', 'block');
+}
+
+function hideModal(){
+  $(modal).css('display', 'none');
 }
 
 $(buttons).click(modalPopup);
+
+$(closeModal).click(hideModal);
+
+
+// Get the modal
+// Get the button that opens the modal
+
+
+// Get the <span> element that closes the modal
+
+// When the user clicks on the button, open the modal
+
+// When the user clicks on <span> (x), close the modal
+
+
+// When the user clicks anywhere outside of the modal, close it
+
+
