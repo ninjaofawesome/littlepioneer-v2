f765386e6b079d41d0d106880b5f6516cd60b3e2
Hannah Nordgren <hey@hereshannahs.info>
2017-04-02 23:52:11 -0400

    updating address and images

--- js/gallery.js
+++ js/gallery.js
@@ -2,24 +2,26 @@ var buttons=$('.artistimage');
 var modal = $('.gallery-modal');
 var closeModal = $('.modal-close');
 var url = 'localhost:8000/galleryUncompressed.html;'
-console.log(url);
 
 function modalPopup(){
   var eachImg = $(this).attr('id').split('-')[1];
-  $.ajax({
-    async: false,
-    url : url,
-    contentType: "text/html",
-    type : "GET",
-    dataType : "text/html",
-    success: function(data){
-      console.log('data', data);
-    },
-    error: function(data, xhr) {
-      console.log('data', data);
-      console.log('xhr', xhr);
-    }
- });
+
+ $.ajax({
+      url: url,
+      type: "POST",
+      crossDomain: true,
+      data: JSON.stringify(somejson),
+      dataType: "json",
+      success: function (response) {
+          var resp = JSON.parse(response)
+          alert(resp.status);
+      },
+      error: function (xhr, status) {
+          alert("error");
+      }
+  });
+
+
 
   $(modal).css('display', 'block');
 }
